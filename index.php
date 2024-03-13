9a0b8f7c9cc9b0628e3277c1d3e02a0bc63f42f5
Theresa Barkasy <theresa.barkasy@gmail.com>
2024-03-13 14:45:34 -0400

    working on links

--- index.php
+++ index.php
@@ -1,19 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="icon" type="image/png" href="changeme" alt="Banjo Bob logo"/>
-    <title>Banjo Bob</title>
-    <link rel="stylesheet" type="text/css" href="css/index.css"/>
-</head>
-<body>
-    <!-- Navbar Section Start -->
-        <?php include('./components/navbar.php'); ?>
-    <!-- Navbar Section End -->
-
-    <!-- Footer Section Start -->
-    <?php include('./components/footer.php'); ?>
-    <!-- Footer Section End -->
-</body>
-</html>
+<!-- Navbar Section Start -->
+<?php include('./components/navbar.php'); ?>
+<!-- Navbar Section End -->
+<div>i am the body</div>
+<!-- Footer Section Start -->
+<?php include('./components/footer.php'); ?>
+<!-- Footer Section End -->
---
+++ pages/listen.php
@@ -0,0 +1,7 @@
+<!-- Navbar Section Start -->
+<?php include('../components/navbar.php'); ?>
+<!-- Navbar Section End -->
+<div>I am the Listen Page</div>
+<!-- Footer Section Start -->
+<?php include('../components/footer.php'); ?>
+<!-- Footer Section End -->
