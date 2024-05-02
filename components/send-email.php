7cae12d2f725049c6effa1ff627fb5cdb634f618
Theresa Barkasy <theresa.barkasy@gmail.com>
2024-05-01 20:39:07 -0400

    composer's autoloader, etc; list group item bs5

--- components/send-email.php
+++ components/send-email.php
@@ -5,6 +5,29 @@ $email = $_POST['email'];
 $subject = $_POST['subject'];
 $message = $_POST['message'];
 
+//uncomment the following after installing php mailer https://www.youtube.com/watch?v=fIYyemqKR58
+// require "vendor/autoload.php";
+
+// use PHPMailer\PHPMailer\PHPMailer;
+// use PHPMailer\PHPMailer\SMTP;
+
+// $mail = new PHPMailer(true);
+
+// $mail->isSMTP();
+// $mail->SMTPAuth = true;
+
+//insert host name of the SMTP server :
+// $mail->Host = "smtp.example.com";
+// $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+// $mail->Port = 587;
+
+// $mail->Username = "you@example.com";
+// $mail->Password = "mypassword";
+
+//
+//
+
+
 ?>
 
 <!-- To Do: -->
