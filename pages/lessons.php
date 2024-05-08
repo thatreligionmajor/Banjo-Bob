d4a216d7a61082f9e138b965d951c122f71dfa9c
Theresa Barkasy <theresa.barkasy@gmail.com>
2024-05-08 12:28:16 -0400

    card bg-dark

--- pages/lessons.php
+++ pages/lessons.php
@@ -87,7 +87,7 @@
         </div>
         <div class="row row-cols-1 row-cols-md-3 g-4">
             <div class="col">
-                <div class="card h-100 bg-light">
+                <div class="card border-dark h-100 bg-light">
                     <div class="bg-success text-white p-3 mb-3 text-center rounded-top">
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person" viewBox="0 0 16 16">
                             <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6m2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0m4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4m-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10s-3.516.68-4.168 1.332c-.678.678-.83 1.418-.832 1.664z" />
@@ -100,7 +100,7 @@
                 </div>
             </div>
             <div class="col">
-                <div class="card h-100 bg-light">
+                <div class="card border-dark h-100 bg-light">
                     <div class="bg-success text-white p-3 mb-3 text-center rounded-top">
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-music-note-list" viewBox="0 0 16 16">
                             <path d="M12 13c0 1.105-1.12 2-2.5 2S7 14.105 7 13s1.12-2 2.5-2 2.5.895 2.5 2" />
@@ -116,7 +116,7 @@
                 </div>
             </div>
             <div class="col">
-                <div class="card h-100 bg-light">
+                <div class="card border-dark sh-100 bg-light">
                     <div class="bg-success text-white p-3 mb-3 text-center rounded-top">
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-music-note-beamed" viewBox="0 0 16 16">
                             <path d="M6 13c0 1.105-1.12 2-2.5 2S1 14.105 1 13s1.12-2 2.5-2 2.5.896 2.5 2m9-2c0 1.105-1.12 2-2.5 2s-2.5-.895-2.5-2 1.12-2 2.5-2 2.5.895 2.5 2" />
