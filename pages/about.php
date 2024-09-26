cedf052f5600ad56e57c41da29d1a44fc83bd9ce
Theresa Barkasy <theresa.barkasy@gmail.com>
2024-09-26 16:07:50 -0400

    switch to light mode

--- pages/about.php
+++ pages/about.php
@@ -6,16 +6,16 @@
 
 <!-- Jumbotron Section Start -->
     <!-- container fluid -->
-    <div class="container-fluid bg-dark rounded p-5 m-3 d-flex justify-content-between">
+    <div class="container-fluid bg-light rounded p-5 m-3 d-flex justify-content-between">
         <div class="row">
             <div class="h-100 p-5 col-3 d-flex align-items-center">
                 <img src="../images/banjo-bob-1976.JPG" alt="Banjo Bob Playing the Banjo" class="img-fluid rounded" />
             </div>
             <div class="h-100 p-5 col-9">
-                <h1 class="text-light">Over 50 Years of Banjo</h1>
+                <h1 class="text-black">Over 50 Years of Banjo</h1>
                 <h5 class="text-success">banjo luthier, player, & performer</h5>
-                <hr class="text-light my-4">
-                <p class="text-light pb-3">
+                <hr class="text-black my-4">
+                <p class="text-black pb-3">
                     <?php echo $your_bio ?>
                 </p>
                 <!-- <a class="btn btn-success btn-lg" href="#" role="button">Learn more</a> -->
