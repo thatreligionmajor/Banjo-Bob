d07523bab7df048a7669068b379c4b1fc4373a85
Theresa Barkasy <theresa.barkasy@gmail.com>
2024-09-26 16:29:29 -0400

    marquee

--- components/image-marquee.php
+++ components/image-marquee.php
@@ -1,19 +1,19 @@
 <!-- Image Marquee Start -->
 <div class="container-fluid bg-light rounded p-5 m-3">
             <div class="row">
-                <div class=" images">
-                    <div class="image-slide">
-                        <div class="d-flex justify-content-start overflow-hidden">
+                <div class="">
+                    <div class="images">
+                        <div class="image-slide d-flex justify-content-start overflow-hidden">
                             <img src="../images/banjo-bob-1976.JPG" alt="Image 1" class="img-thumbnail">
                             <img src="../images/banjo-bob-1976.JPG" alt="Image 2" class="img-thumbnail">
                             <img src="../images/banjo-bob-1976.JPG" alt="Image 3" class="img-thumbnail">
                         </div>
-                        <div class="d-flex justify-content-start overflow-hidden">
+                        <div class="image-slide d-flex justify-content-start overflow-hidden">
                             <img src="../images/banjo-bob-1976.JPG" alt="Image 1" class="img-thumbnail">
                             <img src="../images/banjo-bob-1976.JPG" alt="Image 2" class="img-thumbnail">
                             <img src="../images/banjo-bob-1976.JPG" alt="Image 3" class="img-thumbnail">
                         </div>
-                        <div class="d-flex justify-content-start overflow-hidden">
+                        <div class="image-slide d-flex justify-content-start overflow-hidden">
                             <img src="../images/banjo-bob-1976.JPG" alt="Image 1" class="img-thumbnail">
                             <img src="../images/banjo-bob-1976.JPG" alt="Image 2" class="img-thumbnail">
                             <img src="../images/banjo-bob-1976.JPG" alt="Image 3" class="img-thumbnail">
