0e3af1b9bec4c6e737dc99c45b21d9ee0bd8ef4b
Theresa Barkasy <theresa.barkasy@gmail.com>
2025-05-18 10:46:23 -0400

    arbitrary email

--- pages/contact.php
+++ pages/contact.php
@@ -17,7 +17,7 @@
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-envelope-fill m-1" viewBox="0 0 16 16">
                             <path d="M.05 3.555A2 2 0 0 1 2 2h12a2 2 0 0 1 1.95 1.555L8 8.414zM0 4.697v7.104l5.803-3.558zM6.761 8.83l-6.57 4.027A2 2 0 0 0 2 14h12a2 2 0 0 0 1.808-1.144l-6.57-4.027L8 9.586zm3.436-.586L16 11.801V4.697z" />
                         </svg>
-                        <h5>bob@email.com</h5>
+                        <h5>captainbanjobob@email.com</h5>
                     </div>
                     <div class="list-group-item d-flex w-100 justify-content-center list-group-item-light list-group-item-action">
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-telephone-fill m-1" viewBox="0 0 16 16">
